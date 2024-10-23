ee7c1cdab763aabd83df031ea7fecf12df94ca65
Alunos <nicolastho@hotmail.com>
2024-10-22 22:06:33 -0300

    hooks

---
+++ hooks/exemplo/src/App.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from "react";
+
+function App(){
+  const [tarefas, setTarefas] = useState(
+    ['Pagar a conta de luz', 'Estudar React']
+  );
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('tarefas')
+
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+  function adicionarTarefas(){
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
+  return(
+    <div>
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        )
+
+        )}
+      </ul>
+
+      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+      <button type="button" onClick={adicionarTarefas}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
